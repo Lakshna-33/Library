44814e38b814e88621eb82efc4d22ea52550a99a
lakshna-33 <samshetti38@gmail.com>
2025-09-08 12:54:36 +0530

    Book component holds the details of a single book and passes props as object for BookList components to display as UI

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,17 +2,20 @@ import "./style.css"
 
 function Book(props) {
   return (
+    
     <div className="book-card">
-      <img src= {props.details.coverImage} alt="" width="200px" height="200px" className="book-image" 
-      />
+      <img src= {props.details.coverImage} alt="" width="200px" height="200px" className="book-image" />
+  
         <div className="book-details">
-        <h1 className="book-title">{props.details.title}</h1>
-        <p className="book-genre">{props.details.genre}</p>
+          <div className="book-header">
+  <h2 className="book-title">
+  {props.details.title} <span className="book-genre">({props.details.genre})</span>
+</h2>
+
         <h4 className="book-author">{props.details.author}</h4>
-        <p className="book-description">{props.details.description}</p>
-        <p className="book-year">{props.details.year}</p>
       </div>
-      </div>
+       </div></div>
+       
   )
 }
 
