ed8e1da3f5402470052cc4103b81480ed1b4f8de
lakshna-33 <samshetti38@gmail.com>
2025-09-08 13:22:39 +0530

    Added App that holds the BookList component that handles search books by title in search field

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import BookList from './components/BookList'
 import { Books } from './Data/mockData'
 import "./components/style.css"
 import { useState } from 'react'
+import Header from './components/Header';
 
 
 function App() {
-
+  
   const [searchText, setText] = useState("");
   const [filteredBooks, setfilteredBooks] = useState(Books);
 
@@ -13,18 +14,25 @@ function App() {
      const filterBooks = Books.filter((book) => book.title.toLowerCase().includes(searchText.toLowerCase()));
 
     setfilteredBooks(filterBooks);
+
   }
+  
 
   return (
     <>
+    <Header />
+    <body className = "main-body">
+    
+     
     <div className="search">
        <h2>Search Books</h2>
        <div>
-      <input type = "text" name="" className="search-input" onChange={(e) => setText(e.target.value)} />
+      <input type = "text" name="" className="search-input" placeholder="Search here by book's title..." onChange={(e) => setText(e.target.value)} />
       <button onClick={handleSearch}>Search</button>
       </div>
     </div>
      <BookList data={filteredBooks}/>
+     </body>
     </> 
   )
 }
