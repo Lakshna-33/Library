349671e1bbe5b87a296bcc15c8c3d76c93ab95e0
lakshna-33 <samshetti38@gmail.com>
2025-09-08 12:43:35 +0530

    Added landing page that shows a welcome message and header component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import "./WelcomePage.css"
 
 function Welcome() {
   return (
     <>
     <Header/>
-    <div className = "Welcome-container">
+    <body className = "welcome-container">
         <h1 className="welcome-title">Welcome to the Online Library System with Exciting Fiction and Non-Fiction Categories</h1>
         <p className="welcome-text">Please Choose a category!!!for our Unique Collections</p>
-    </div>
+    </body>
     </>
   )
 }
