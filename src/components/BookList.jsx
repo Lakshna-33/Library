e1b5c91180a39342c809cf4b528e1a5a7a7f3c38
lakshna-33 <samshetti38@gmail.com>
2025-09-08 12:52:08 +0530

    Added all the books of all categories from the bokk component under home URL in header

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,27 @@
-import Book from "./Book"
-import "./style.css"
+import Book from "./Book";
+import "./style.css";
+import { Link } from "react-router-dom";
 
 function BookList(props) {
-    return (
-      <div className = "bookList">
-        {
-      props.data.map((data) => (<Book key={data.id} details={data}/> )) 
-        }
-  
-      </div>
-    )
+  return (
+    <div className="book-list">
+      {props.data.map((data) => (
+        <div key={data.id}>
+           {/* Normal Book card */}
+          {/* <Book details={data} /> */}
+
+          {/* Linked Book card */}
+          <Book details ={{...data, title :(
+            <Link to={`/book/${data.id}`} className="title-link">
+            {data.title}
+          </Link>
+          )
+        }}
+        />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default BookList
