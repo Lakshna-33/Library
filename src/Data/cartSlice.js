ae202ecb29a57f45b8be93b656a3bdef05d626b7
lakshna-33 <samshetti38@gmail.com>
2025-09-08 13:09:08 +0530

    added cartSlice Reducer with states and actions with functions addtocart, removefromcart and clearcart reducers

---
+++ src/Data/cartSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name : "cart",
+    initialState : {
+        items: [],
+    },
+    reducers:{
+        addtocart : (state,action) => {
+            state.items.push(action.payload);
+        },
+
+        removefromcart : (state,action) => {
+            const idToRemove = action.payload; // EXPECTS just the id (number/string)
+      const index = state.items.findIndex((item) => item.id === idToRemove);
+
+      if (index === -1) return; // nothing to remove
+
+      if (state.items[index].quantity > 1) {
+        // reduce quantity by one
+        state.items[index].quantity -= 1;
+      } else {
+        // quantity === 1 -> remove the item completely
+        state.items.splice(index, 1);
+      }
+    },
+
+        clearcart : (state) => {
+            state.items.length = 0;
+    },
+ },
+    });
+
+
+    export default cartSlice.reducer;
+    export const { addtocart, removefromcart, clearcart } = cartSlice.actions;
