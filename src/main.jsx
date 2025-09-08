3214ce7e1c392268d9db925e9253a19f0f39a98f
lakshna-33 <samshetti38@gmail.com>
2025-09-08 13:25:36 +0530

    Entry file, renders App with React DOM and appRouter that contains all the routes with all paths and its elements and Provider that provides store configuration to the entire App

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
+import React from "react"
 import App from './App.jsx'
 import "./App.css"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import PageNotFound from "./components/PageNotFound.jsx"
+import BrowseBooks from "./components/BrowseBooks.jsx"
+import Welcome from "./components/Welcome.jsx"
+import Bookdetails from "./components/Bookdetails.jsx"
+import { Provider } from "react-redux"
+import appstore from "./Data/appstore.js"
+import CartPage from "./components/CartPage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Welcome/>,
+  },
+    {
+    path: "/home",
+    element: <App />,
+  },
+  {
+    path: "/browsebooks",
+    element: <BrowseBooks />,
+  },
+  {
+    path: "/book/:id",
+    element: <Bookdetails/>
+  },
+  {
+    path: "/cartpage",
+    element :  <CartPage/>
+  },
+  {
+    path:"*",
+    element:<PageNotFound/>
+  },
+
+  ]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={appstore}>
+    <RouterProvider router = {appRouter} />
+  </Provider>,
 )
